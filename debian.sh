b000e27bbcc5584742d7b3640d40c812d8f2ced7
Caio Rosa <>
2020-11-27 10:03:44 -0300

    Uncomment debian install script commands

--- debian.sh
+++ debian.sh
@@ -1,21 +1,21 @@
 if !command -v ansible-playbook &> /dev/null; then
   echo 'Ansible is not installed, installing...'
-  #sudo apt update
-  #sudo apt install software-properties-common
-  #sudo apt-add-repository --yes --update ppa:ansible/ansible
-  #sudo apt install ansible
+  sudo apt update
+  sudo apt install software-properties-common
+  sudo apt-add-repository --yes --update ppa:ansible/ansible
+  sudo apt install ansible
 fi
 
 echo 'Ansible ready, starting installation.'
-#ansible-playbook yggdrasil.yml --ask-become-pass
+ansible-playbook yggdrasil.yml --ask-become-pass
 
 echo 'Do you want to generate git ssh key?'
 select permit in "Yes" "No"; do
   case $permit in
     Yes )
       echo 'Using the default values, you can change them in vars/external_vars.yml'
-      #ansible-galaxy collection install community.crypto
-      #ansible-playbook gitssh.yml
+      ansible-galaxy collection install community.crypto
+      ansible-playbook gitssh.yml
       break;;
     No ) break;;
   esac
