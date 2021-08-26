cf0fec797d6bbb59f55013a619a025c21265e24f
Caio Rosa <>
2021-08-26 14:55:49 -0300

    Open taskwarrior-tui automatically in tmux task window

--- roles/tmux/files/starttmux.sh
+++ roles/tmux/files/starttmux.sh
@@ -8,7 +8,10 @@ tmux select-window -t $session:0
 tmux rename-window nvim
 
 tmux new-window -t $session:1 -n dev
+
 tmux new-window -t $session:2 -n task
+tmux select-window -t $session:2
+tmux send-keys -t $session:2 "taskwarrior-tui" ENTER
 
 tmux select-window -t $session:0
 
