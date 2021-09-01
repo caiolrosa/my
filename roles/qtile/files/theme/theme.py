521a69d83a7464d000c51f25b3a2bd49b97b38dd
Caio Rosa <caiolrosa@users.noreply.github.com>
2021-09-01 03:10:14 -0300

    Qtile bar improvements

---
+++ roles/qtile/files/theme/theme.py
@@ -0,0 +1,15 @@
+colors = {
+    "darkest_gray": "#2e3440",
+    "darker_gray": "#3b4252",
+    "dark_gray": "#434c5e",
+    "medium_gray": "#4c566a",
+    "light_gray": "#d8dee9",
+    "accent_blue": "#88c0d0",
+    "accent_green": "#8fbcbb",
+    "light_green": "#a3be8c",
+    "darker_blue": "#81a1c1",
+    "darkest_blue": "#5e81ac",
+    "purple": "#b48ead",
+    "orange": "#d08770",
+    "yellow": "#ebcb8b"
+}
