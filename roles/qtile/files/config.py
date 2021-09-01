403b419393549498058b9a54f25ad18d26c2eee6
Caio Rosa <>
2021-08-31 23:54:55 -0300

    Add base qtile config

---
+++ roles/qtile/files/config.py
@@ -0,0 +1,242 @@
+from typing import List  # noqa: F401
+
+from libqtile import bar, layout, widget, hook
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
+from libqtile.lazy import lazy
+
+from custom_widgets import volume
+
+import os
+import subprocess
+
+mod = "mod1"
+terminal = "alacritty"
+
+keys = [
+    # Switch between windows
+    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+
+    Key([mod], "space", lazy.spawn("dmenu_run"), desc="Spawn dmenu"),
+
+    # Move windows between left/right columns or move up/down in current stack.
+    # Moving out of range in Columns layout will create new column.
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
+        desc="Move window to the left"),
+    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
+        desc="Move window to the right"),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
+        desc="Move window down"),
+    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
+    # Grow windows. If current window is on the edge of screen and direction
+    # will be to screen edge - window would shrink.
+    Key([mod, "control"], "h", lazy.layout.grow_left(),
+        desc="Grow window to the left"),
+    Key([mod, "control"], "l", lazy.layout.grow_right(),
+        desc="Grow window to the right"),
+    Key([mod, "control"], "j", lazy.layout.grow_down(),
+        desc="Grow window down"),
+    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+    # Toggle between split and unsplit sides of stack.
+    # Split = all windows displayed
+    # Unsplit = 1 window displayed, like Max layout, but still with
+    # multiple stack panes
+    Key([mod], "Return", lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack"),
+    Key([mod, "shift"], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+
+    # Toggle between different layouts as defined below
+    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
+
+    Key([mod], "q", lazy.restart(), desc="Restart Qtile"),
+    Key([mod, "shift"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+    Key([mod], "r", lazy.spawncmd(),
+        desc="Spawn a command using a prompt widget"),
+    
+    # Audio keybinds
+    Key([mod], "v", lazy.spawn("amixer -q set Master 5%+"), desc="Increase volume"),
+    Key([mod, "shift"], "v", lazy.spawn("amixer -q set Master 5%-"), desc="Decrease volume"),
+    Key([mod, "shift"], "m", lazy.spawn("amixer set Master toggle"), desc="Mute volume")
+]
+
+group_names = ["web", "terminal", "chat", "music", "misc"]
+groups = [Group(i) for i in group_names]
+
+for i, group in enumerate(groups, 1):
+    keys.extend([
+        # mod1 + letter of group = switch to group
+        Key([mod], str(i), lazy.group[group.name].toscreen(),
+            desc="Switch to group {}".format(group.name)),
+
+        # mod1 + shift + letter of group = switch to & move focused window to group
+        Key([mod, "shift"], str(i), lazy.window.togroup(group.name, switch_group=True),
+            desc="Switch to & move focused window to group {}".format(group.name)),
+        # Or, use below if you prefer not to switch to that group.
+        # # mod1 + shift + letter of group = move focused window to group
+        # Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
+        #     desc="move focused window to group {}".format(i.name)),
+    ])
+
+colors = {
+    "darkest_gray": "#2e3440",
+    "darker_gray": "#3b4252",
+    "dark_gray": "#434c5e",
+    "medium_gray": "#4c566a",
+    "light_gray": "#d8dee9",
+    "accent_blue": "#88c0d0",
+    "accent_green": "#8fbcbb",
+    "light_green": "#a3be8c",
+    "darker_blue": "#81a1c1",
+    "darkest_blue": "#5e81ac",
+    "purple": "#b48ead",
+    "orange": "#d08770",
+    "yellow": "#ebcb8b"
+}
+
+layouts = [
+    layout.Columns(
+        margin = 12,
+        border_width = 2,
+        border_focus = colors["accent_green"],
+        border_on_sigle = True
+    ),
+    # layout.Max(),
+    # Try more layouts by unleashing below layouts.
+    # layout.Stack(num_stacks=2),
+    # layout.Bsp(),
+    # layout.Matrix(),
+    # layout.MonadTall(),
+    # layout.MonadWide(),
+    # layout.RatioTile(),
+    # layout.Tile(),
+    # layout.TreeTab(),
+    # layout.VerticalTile(),
+    # layout.Zoomy(),
+]
+
+widget_defaults = dict(
+    font='Ubuntu Bold',
+    fontsize=13,
+    padding=6,
+    background=colors["darkest_gray"]
+)
+extension_defaults = widget_defaults.copy()
+
+screens = [
+    Screen(
+        top=bar.Bar(
+            [
+                widget.Spacer(length = 12),
+                widget.Image(
+                    filename = "~/.config/qtile/arch_icon.png",
+                ),
+                widget.Spacer(length = 6),
+                widget.GroupBox(
+                    active = colors["accent_green"],
+                    inactive = colors["darker_blue"],
+                    block_highlight_text_color = colors["darkest_gray"],
+                    highlight_color = colors["accent_blue"],
+                    highlight_method = "block",
+                    this_current_screen_border = colors["accent_blue"],
+                    this_current_border = colors["accent_blue"],
+                    foreground = colors["light_gray"],
+                    background = colors["darkest_gray"]
+                ),
+                widget.Sep(
+                    foreground = colors["dark_gray"],
+                    linewidth = 2
+                ),
+                widget.Spacer(length = 6),
+                widget.WindowName(
+                    foreground = colors["purple"]
+                ),
+                widget.CPU(
+                    foreground = colors["accent_green"],
+                    format = " {load_percent}%"
+                ),
+                widget.Memory(
+                    foreground = colors["accent_blue"],
+                    format = " {MemPercent}%"
+                ),
+                volume.Volume(
+                    foreground = colors["light_green"],
+                ),
+                widget.Clock(
+                    foreground = colors["orange"],
+                    format=" %H:%M"
+                ),
+                widget.Clock(
+                    foreground = colors["darkest_blue"],
+                    format=" %a %b %_d"
+                ),
+                widget.CheckUpdates(
+                    display_format = " {updates} updates",
+                    no_update_string = " 0 updates",
+                    colour_have_updates = colors["yellow"],
+                    colour_no_updates = colors["darker_blue"],
+                    update_interval = 1800
+                ),
+                widget.TextBox(
+                    foreground = colors['light_gray'],
+                    text = "{} {}".format(os.uname().sysname, os.uname().nodename)
+                ),
+                widget.Spacer(length = 8)
+            ],
+            24,
+        ),
+    ),
+]
+
+# Drag floating layouts.
+mouse = [
+    Drag([mod], "Button1", lazy.window.set_position_floating(),
+         start=lazy.window.get_position()),
+    Drag([mod], "Button3", lazy.window.set_size_floating(),
+         start=lazy.window.get_size()),
+    Click([mod], "Button2", lazy.window.bring_to_front())
+]
+
+dgroups_key_binder = None
+dgroups_app_rules = []  # type: List
+follow_mouse_focus = True
+bring_front_click = False
+cursor_warp = False
+floating_layout = layout.Floating(float_rules=[
+    # Run the utility of `xprop` to see the wm class and name of an X client.
+    *layout.Floating.default_float_rules,
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
+])
+auto_fullscreen = True
+focus_on_window_activation = "smart"
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+auto_minimize = True
+
+@hook.subscribe.startup_once
+def start_once():
+    home = os.path.expanduser('~')
+    subprocess.call([home + '/.config/qtile/startup.sh'])
+
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+# string besides java UI toolkits; you can see several discussions on the
+# mailing lists, GitHub issues, and other WM documentation that suggest setting
+# this string if your java app doesn't work correctly. We may as well just lie
+# and say that we're a working one by default.
+#
+# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in
+# java that happens to be on java's whitelist.
+wmname = "LG3D"
