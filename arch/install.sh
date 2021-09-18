f93f98e4d63f1dcf512d08d1126b877706fe9734
Caio Rosa <>
2021-09-18 03:05:30 -0300

    Separate software installation functions

---
+++ arch/install.sh
@@ -0,0 +1,57 @@
+alacritty() {
+	echo Alacritty
+}
+
+oh_my_zsh() {
+	echo OhMyZsh
+}
+
+tmux() {
+	echo Tmux
+}
+
+nerd_fonts() {
+	echo Nerd Fonts
+}
+
+direnv() {
+	echo Direnv
+}
+
+taskwarrior() {
+	echo Taskwarrior
+}
+
+nvim() {
+	echo Nvim
+}
+
+asdf() {
+	echo Asdf
+}
+
+qtile() {
+	echo Qtile
+}
+
+xmonad() {
+	echo Xmonad
+}
+
+betterlockscreen() {
+	echo BetterLockScreen
+}
+
+brave() {
+	echo Brave
+}
+
+google_chrome() {
+	echo Google Chrome
+}
+
+docker() {
+	echo Docker
+}
+
+$1
