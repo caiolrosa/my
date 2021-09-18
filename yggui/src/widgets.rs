6603d85b12b6de5e9d64e8f422f8bbdb87532f23
Caio Rosa <>
2021-09-18 01:43:01 -0300

    Add confirm widget and allow passing title to checklist

--- yggui/src/widgets.rs
+++ yggui/src/widgets.rs
@@ -1,4 +1,4 @@
-use dialoguer::{MultiSelect, theme::ColorfulTheme};
+use dialoguer::{Confirm, MultiSelect, theme::ColorfulTheme};
 
 pub fn checklist(title: &str, options: Vec<String>) -> Option<Vec<usize>> {
     let defaults: Vec<bool> = options.iter().map(|_| true).collect();
@@ -8,4 +8,11 @@ pub fn checklist(title: &str, options: Vec<String>) -> Option<Vec<usize>> {
         .items(&options)
         .defaults(&defaults)
         .interact().ok()
+}
+
+pub fn confirm(title: &str) -> Option<bool> {
+    Confirm::with_theme(&ColorfulTheme::default())
+        .with_prompt(title)
+        .interact()
+        .ok()
 }
