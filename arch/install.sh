be5862ca5427d60b582d7075eefc12bb4bc9bf17
Caio Rosa <>
2021-09-19 11:19:19 -0300

    Fix missing sudo

--- arch/install.sh
+++ arch/install.sh
@@ -45,8 +45,8 @@ tmux() {
 
 nerd_fonts() {
 	wget "https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/SourceCodePro.zip"
-	mkdir -p /usr/share/fonts/TTF
-	unzip SourceCodePro.zip -d /usr/share/fonts/TTF
+	sudo mkdir -p /usr/share/fonts/TTF
+	sudo unzip SourceCodePro.zip -d /usr/share/fonts/TTF
 	rm SourceCodePro.zip
 
 	return 0
