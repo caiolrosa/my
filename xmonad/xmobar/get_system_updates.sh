0703b2e55ca4d08b307814498b0abc71759659c3
Caio Rosa <>
2022-07-27 23:35:02 -0300

    Update yggui deps and remove qtile and xmonad

--- xmonad/xmobar/get_system_updates.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-total=$(checkupdates | wc -l)
-if [[ $total -eq 0 ]]; then
-  echo "<fc=#81a1c1><fn=2></fn>  $total updates</fc>"
-else
-  echo "<fc=#ebcb8b><fn=2></fn>  $total updates</fc>"
-fi
