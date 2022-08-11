941d9cd6b56dfcbf9611d8a476ea5e8c314462f8
Caio Rosa <caiolrosa@users.noreply.github.com>
2022-08-11 20:23:40 -0300

    Fix wifi widget formatting

--- awesome/wifi_signal.sh
+++ awesome/wifi_signal.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-iwconfig $1 | grep -i quality | awk '{printf $2}' | awk -F '=' '{printf $2}' | awk -F '/' '{printf "%.0f", ($1/$2)*100}'
+iwconfig $1 | grep -i quality | awk '{printf $2}' | awk -F '=' '{printf $2}' | awk -F '/' '{printf "%.0f\n", ($1/$2)*100}'
