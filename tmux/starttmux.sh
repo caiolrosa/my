6b61378e9bdf15a2ea439b76801a22278f86bcc1
Caio Rosa <caiolrosa1912@gmail.com>
2022-08-11 23:31:49 -0300

    Remove taskwarrior from tmux startup

--- tmux/starttmux.sh
+++ tmux/starttmux.sh
@@ -10,10 +10,6 @@ tmux send-keys -t $session:0 "nvim" ENTER
 
 tmux new-window -t $session:1 -n dev
 
-tmux new-window -t $session:2 -n task
-tmux select-window -t $session:2
-tmux send-keys -t $session:2 "taskwarrior-tui" ENTER
-
 tmux select-window -t $session:0
 
 tmux attach-session -t $session
