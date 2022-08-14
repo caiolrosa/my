8c4ebb9938abb6b0429b3d1d16f6611c42705b78
Caio Rosa <caiolrosa@users.noreply.github.com>
2022-08-14 19:30:56 -0300

    Symlink alacritty config

--- ubuntu/alacritty.sh
+++ ubuntu/alacritty.sh
@@ -7,9 +7,11 @@ sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfi
 
 cargo build --release
 
-sudo cp target/release/alacritty /usr/local/bin # or anywhere else in $PATH
+sudo cp target/release/alacritty /usr/local/bin
 sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
 
 cd - && rm -r /tmp/alacritty
+
+ln -s $HOME/yggdrasil/alacritty $HOME/.config
