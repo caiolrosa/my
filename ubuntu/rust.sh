94bf719400334c673bc6bd94baf6951b8b98c627
Caio Rosa <caiolrosa1912@gmail.com>
2022-08-14 17:53:35 -0300

    Fix zsh installation

--- ubuntu/rust.sh
+++ ubuntu/rust.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-source $HOME/.zshrc
 asdf plugin-add rust https://github.com/asdf-community/asdf-rust.git
 asdf install rust latest
 asdf global rust latest
--- ubuntu/zsh.sh
+++ ubuntu/zsh.sh
@@ -3,17 +3,3 @@
 sudo apt install zsh
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-
-if [ -f "$HOME/.zshrc" ]; then
-	mv $HOME/.zshrc $HOME/.zshrc_bak
-	ln -s $HOME/yggdrasil/oh_my_zsh/.zshrc $HOME
-	exit 3
-fi
-
-ln -s $HOME/yggdrasil/oh_my_zsh/.zshrc $HOME
-
-git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-chsh -s $(which zsh)
