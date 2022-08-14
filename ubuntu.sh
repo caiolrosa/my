3bb5044434bd8bba0d61457cd481e19d02a5d0fd
Caio Rosa <caiolrosa@users.noreply.github.com>
2022-08-14 17:56:41 -0300

    Separate zsh installation

--- ubuntu.sh
+++ ubuntu.sh
@@ -10,21 +10,6 @@ if [ $? -ne 0 ]; then
 	echo "Failed installing baseline packages"; exit 1
 fi
 
-if ! hash zsh; then
-	echo -e "\n>>>>> Installing zsh <<<<<\n"
-	bash ubuntu/zsh.sh
-	echo -e "\n>>>>> Finished installing zsh <<<<<\n"
-else
-	ln -s $HOME/yggdrasil/oh_my_zsh/.zshrc $HOME
-
-	git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-
-	source $HOME/.zshrc
-fi
-
-
 IFS=","
 
 apps_title="Which applications should be installed?"
