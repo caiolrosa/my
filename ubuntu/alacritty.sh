670da0fee040f6e3806b1a4f7ffc7829c7d6638b
Caio Rosa <caiolrosa@users.noreply.github.com>
2022-08-14 17:41:28 -0300

    Fix rust installation and alacritty build

--- ubuntu/alacritty.sh
+++ ubuntu/alacritty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git clone https://github.com/alacritty/alacritty.git
-cd alacritty
+git clone https://github.com/alacritty/alacritty.git /tmp/alacritty
+cd /tmp/alacritty
 
 sudo apt install cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 
@@ -12,4 +12,4 @@ sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
 
-cd .. && rm -r alacritty
+cd - && rm -r /tmp/alacritty
