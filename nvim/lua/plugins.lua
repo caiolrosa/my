bf1f44043e535be33986897a7cc0a61870ca6672
Caio Rosa <caiolrosa@users.noreply.github.com>
2022-10-10 15:33:11 -0300

    Remove unused plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,17 +4,14 @@ require('packer').startup(function()
   use 'wbthomason/packer.nvim'
 
   use 'shaunsingh/nord.nvim'
-  use 'edkolev/tmuxline.vim'
   use { 'neoclide/coc.nvim', branch = 'release', run = function() vim.fn['coc#util#install']() end }
   use 'tpope/vim-endwise'
   use 'Raimondi/delimitMate'
   use 'alvan/vim-closetag'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-surround'
-  use 'dense-analysis/ale'
   use { 'iamcco/markdown-preview.nvim', run = 'cd app && yarn install' }
   use 'vimwiki/vimwiki'
-  use 'mhinz/vim-startify'
 
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
   use { 'hoob3rt/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
@@ -31,4 +28,3 @@ require('plugins.telescope')
 require('plugins.coc')
 require('plugins.treesitter')
 require('plugins.vim_close_tag')
-require('plugins.startify')
