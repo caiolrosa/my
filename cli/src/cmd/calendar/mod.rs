a8c473e33f8d6431ff44766c9aa51abf0c7f7c17
Caio Rosa <caiolrosa@users.noreply.github.com>
2022-10-24 17:52:06 -0300

    Add doc comments to display command descriptions

--- cli/src/cmd/calendar/mod.rs
+++ cli/src/cmd/calendar/mod.rs
@@ -4,7 +4,10 @@ use clap::{Parser, Subcommand};
 
 #[derive(Subcommand)]
 enum CalendarCommand {
+    /// List all items in the calendar
     List,
+
+    /// Syncronize calendar across all registered accounts
     Sync,
 }
 
