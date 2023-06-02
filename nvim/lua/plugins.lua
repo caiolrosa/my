cf6963e31053362c2f62864811c249e8fb31cc58
Caio Rosa <caiolrosa@users.noreply.github.com>
2023-06-02 11:48:54 -0300

    initial lsp zero setup

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,29 +9,38 @@ require('packer').startup(function()
       ts_update()
     end,
   }
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use 'neovim/nvim-lspconfig'
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
   use 'windwp/nvim-autopairs'
   use 'windwp/nvim-ts-autotag'
-  use { "glepnir/lspsaga.nvim", branch = "main", requires = { { "nvim-tree/nvim-web-devicons" } } }
 
   use 'shaunsingh/nord.nvim'
 
   use 'tpope/vim-surround'
   use 'tpope/vim-fugitive'
 
+  use 'nvim-tree/nvim-web-devicons'
+
   use { 'hoob3rt/lualine.nvim', requires = { 'nvim-tree/nvim-web-devicons', opt = true } }
   use { 'akinsho/bufferline.nvim', requires = 'nvim-tree/nvim-web-devicons' }
   use { 'nvim-tree/nvim-tree.lua', requires = 'nvim-tree/nvim-web-devicons' }
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
 
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
+      {'neovim/nvim-lspconfig'},
+      {
+        'williamboman/mason.nvim',
+        run = function() pcall(vim.cmd, 'MasonUpdate') end
+      },
+      {'williamboman/mason-lspconfig.nvim'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'L3MON4D3/LuaSnip'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'}
+    }
+  }
 end)
 
 require('plugins.colorscheme')
@@ -40,7 +49,4 @@ require('plugins.tree')
 require('plugins.telescope')
 require('plugins.treesitter')
 require('plugins.autopairs')
-require('plugins.mason')
 require('plugins.lsp')
-require('plugins.cmp')
-require('plugins.saga')
