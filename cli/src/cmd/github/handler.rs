d91ce6bb0935a892c32c696d790ae60a9e00162a
Caio Rosa <caiolrosa@users.noreply.github.com>
2024-04-08 11:03:40 -0300

    remove cli

--- cli/src/cmd/github/handler.rs
+++
@@ -1,9 +0,0 @@
-use super::{GithubHandler, GithubCommand};
-
-impl GithubHandler {
-    pub fn handle(&self) {
-        match &self.command {
-            GithubCommand::Prs => println!("Github prs command")
-        }
-    }
-}
