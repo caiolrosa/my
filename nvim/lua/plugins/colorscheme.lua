4212ecf7f65659b883a5c0851d60b605bc11af37
Caio Rosa <>
2024-04-08 13:26:28 -0300

    setup lazyvim

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,6 +1,9 @@
-vim.g.nord_disable_background = true
-vim.g.nord_contrast = true
-vim.g.nord_borders = true
-vim.g.nord_bold = false
-
-require('nord').set()
+return {
+  { 'shaunsingh/nord.nvim' },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  }
+}
