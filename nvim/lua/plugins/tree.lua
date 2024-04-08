2dc7a350ffa993cd95bfcd3035a5f72fb9827fa5
Caio Rosa <caiolrosa@users.noreply.github.com>
2024-04-08 13:26:28 -0300

    setup lazyvim

--- nvim/lua/plugins/tree.lua
+++
@@ -1,8 +0,0 @@
-require('nvim-tree').setup()
-local opts = { noremap = true, silent = true }
-vim.api.nvim_set_keymap('n', '<C-n>', ':NvimTreeToggle<CR>', opts)
-vim.api.nvim_set_keymap('n', '<leader>r', ':NvimTreeRefresh<CR>', opts)
-vim.api.nvim_set_keymap('n', '<C-l>', ':NvimTreeFindFile<CR>', opts)
-
-vim.g.nvim_tree_quit_on_open = 1
-vim.g.nvim_tree_git_hl = 1
