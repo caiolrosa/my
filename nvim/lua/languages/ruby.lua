5ee45afffc23d71a6b890bf63fe7a5e298d766cf
Caio Rosa <caiolrosa@users.noreply.github.com>
2024-09-11 12:28:17 -0300

    updates and fix ruby config

--- nvim/lua/languages/ruby.lua
+++ nvim/lua/languages/ruby.lua
@@ -12,7 +12,20 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "standardrb",
+        "solargraph",
       })
     end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        solargraph = {
+          init_options = {
+            formatting = false,
+          },
+        },
+      },
+    },
   },
 }
