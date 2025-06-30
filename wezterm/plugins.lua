950c165bc1594f822f420337109268436b96fe80
Caio Rosa <caiolrosa@users.noreply.github.com>
2025-06-30 14:50:41 -0300

    add wezterm config

---
+++ wezterm/plugins.lua
@@ -0,0 +1,21 @@
+local function setup()
+	local wezterm = require("wezterm")
+	local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
+	tabline.setup({
+		options = {
+			theme = "nord",
+			theme_overrides = {
+				tab = {
+					active = { fg = "#2e3440", bg = "#81a1c1" },
+				},
+			},
+		},
+		sections = {
+			tabline_a = { " " },
+			tabline_y = { "datetime" },
+			tabline_z = { " " },
+		},
+	})
+end
+
+return { setup = setup }
